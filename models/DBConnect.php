482fb5f0dfc97aa94fc4e43e5831e20004a84f6d
43TH3R <michal.fikar@gmail.com>
2015-12-27 12:19:24 +0100

    iocAdd now returns id of inserted entry

--- models/DBConnect.php
+++ models/DBConnect.php
@@ -64,12 +64,13 @@ class DBConnect {
         if (!$stmt->close())
             throw new Exception('Error closing statement [' . $stmt->error . ']');
 
-        return $result->fetch_all(MYSQLI_ASSOC);
+        return $result->fetch_all(MYSQLI_ASSOC)[0];
     }
     
     public function iocAdd($name, $type, $value, $value2) {
         // add new indicator to the `indicators` table
         // table structure: id name type value value2
+        // returns the newly generated id
         if ($value2 == '') $value2 = NULL;
         
         $sql = 'INSERT INTO `indicators` '.
@@ -85,12 +86,24 @@ class DBConnect {
         if (!$stmt->execute())
             throw new Exception('Error executing statement [' . $stmt->error . ']');
         
-        $res = array('rows' => $stmt->affected_rows);
-
         if (!$stmt->close())
             throw new Exception('Error closing statement [' . $stmt->error . ']');
 
-        return $res;
+        $sql = 'SELECT LAST_INSERT_ID();';
+        
+        if (!$stmt = $this->db->prepare($sql))
+            throw new Exception('Error preparing statement [' . $this->db->error . ']');
+
+        if (!$stmt->execute())
+            throw new Exception('Error executing statement [' . $stmt->error . ']');
+        
+        if (!$result = $stmt->get_result())
+            throw new Exception('Error getting result [' . $stmt->error . ']');
+            
+        if (!$stmt->close())
+            throw new Exception('Error closing statement [' . $stmt->error . ']');
+
+        return ['id' => $result->fetch_all(MYSQLI_ASSOC)[0]['LAST_INSERT_ID()']];
     }
     
     public function iocEdit($id, $name, $type, $value, $value2, $hidden) {
@@ -111,7 +124,7 @@ class DBConnect {
         if (!$stmt->execute())
             throw new Exception('Error executing statement [' . $stmt->error . ']');
         
-        $res = array('rows' => $stmt->affected_rows);
+        $res = ['rows' => $stmt->affected_rows];
 
         if (!$stmt->close())
             throw new Exception('Error closing statement [' . $stmt->error . ']');
